2f5aa4af9b3694cc9d5797f846f5cb438596f887
fivaz <s.a.fivaz@gmail.com>
2023-08-16 17:45:08 +0200

    wip homepage

---
+++ src/app/dismiss-button.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export default function DismissButton() {
+  const router = useRouter();
+
+  return (
+    <button
+      className="contents underline text-blue-600"
+      onClick={() => {
+        router.refresh();
+      }}>
+      Dismiss →
+    </button>
+  );
+}
