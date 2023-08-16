57e5cdb2e060a92cebbc889a897b4667768d37f3
fivaz <s.a.fivaz@gmail.com>
2023-08-16 15:18:06 +0200

    add plop generator

---
+++ generators/plopfile.js
@@ -0,0 +1,42 @@
+export default function (plop) {
+    plop.setGenerator('component', {
+        description: 'create new component structure',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Component name',
+            },
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: '../src/components/{{pascalCase name}}/index.tsx',
+                templateFile: 'templates/component.tsx.hbs',
+            },
+            {
+                type: 'add',
+                path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+                templateFile: 'templates/component-stories.tsx.hbs',
+            },
+        ],
+    });
+
+    plop.setGenerator('page', {
+        description: 'create new page structure',
+        prompts: [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'page name',
+            },
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: '../src/pages/{{dashCase name}}.tsx',
+                templateFile: 'templates/page.tsx.hbs',
+            },
+        ],
+    });
+}
